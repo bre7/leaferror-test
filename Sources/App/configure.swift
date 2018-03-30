d20587b5276e0c7fee77ebfbfe9a6b7c3164535c
Nathan <bre7@users.noreply.github.com>
2018-03-30 12:24:58 -0300

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,6 +9,7 @@ public func configure(
     _ env: inout Environment,
     _ services: inout Services
 ) throws {
+    config.prefer(LeafRenderer.self, for: TemplateRenderer.self)
     /// Register providers first
     try services.register(LeafProvider())
     services.register { worker in
@@ -24,7 +25,7 @@ public func configure(
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(DateMiddleware.self) // Adds `Date` header to responses
-    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+//    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     middlewares.use(LeafErrorMiddleware.self)
     services.register(middlewares)
 
